49eb609cf595d6927430d72c208fac74e75f8442
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2025-03-25 16:32:51 +0530

    Update N-queens.java

--- Backtracking/N-queens.java
+++ Backtracking/N-queens.java
@@ -1,39 +1,41 @@
-public class Nqueens 
-{
-    static int[] x;
-    public static boolean place(int k, int i) 
-  {
-        for (int j = 1; j < k; j++)
-            if (x[j] == i || Math.abs(x[j] - i) == Math.abs(j - k))
+import java.util.Scanner;
+public class Nqueens {
+    static int []x;
+    public static boolean Place(int k,int i)
+    {
+        for(int j=1;j<k;j++)
+        {
+            if(x[j]==i || Math.abs(x[j]-i)==Math.abs(j-k))
                 return false;
+        }
         return true;
     }
-
-    public static void nqueens(int k, int n)
-  {
-        if (k > n) 
+    public static void nqueens(int k,int n)
+    {
+        if(k>n)
         {
-            for (int i = 1; i <= n; i++)
-                System.out.print(x[i] + " ");
+            for(int i=1;i<=n;i++)
+                System.out.print(x[i]+" ");
             System.out.println();
-        } 
-        else 
-        {
-            for (int i = 1; i <= n; i++)
-                if (place(k, i)) {
-                    x[k] = i;
-                    nqueens(k + 1, n);
-                }
         }
-    }
-
-    public static void main(String[] args) 
-  {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("Enter number of queens: ");
-        int n = scanner.nextInt();
-        scanner.close();
-        x = new int[n + 1];
+        else{
+            for(int i=1;i<=n;i++)
+            {
+                if(Place(k,i))
+                {
+                    x[k]=i;
+                    nqueens(k+1, n);
+                }
+            }
+        }
+    } 
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of queens:");
+        int n=sc.nextInt();
+        x=new int[n+1];
         nqueens(1, n);
-    }
+        sc.close();
+    }    
 }
